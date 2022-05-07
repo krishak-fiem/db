8b9411afc39acb4bfa9705404b1c74236d24e3b6
Yash Sharma <yashsharma170898@gmail.com>
2022-05-08 01:18:06 +0530

    added cassandra

---
+++ go/cassandra/cassandra.go
@@ -0,0 +1,74 @@
+package cassandra
+
+import (
+	"github.com/gocql/gocql"
+	"github.com/krishak-fiem/utils/go/env"
+	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/table"
+	"log"
+)
+
+var Session gocqlx.Session
+
+func init() {
+	cluster := gocql.NewCluster(env.Get("CASSANDRA_HOST"))
+
+	session, err := gocqlx.WrapSession(cluster.CreateSession())
+	if err != nil {
+		log.Fatal(err)
+	}
+	Session = session
+}
+
+func CreateMetadata(tableName string, columns []string, partKey []string, sortKey []string) table.Metadata {
+	return table.Metadata{
+		Name:    tableName,
+		Columns: columns,
+		PartKey: partKey,
+		SortKey: sortKey,
+	}
+}
+
+func CreateTable(metaData table.Metadata) *table.Table {
+	return table.New(metaData)
+}
+
+func InsertRow(table table.Table, record interface{}) error {
+	q := Session.Query(table.Insert()).BindStruct(record)
+	if err := q.ExecRelease(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetRow(table table.Table, record interface{}) error {
+	q := Session.Query(table.Get()).BindStruct(record)
+	if err := q.GetRelease(&record); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetRows(table table.Table, query map[string]interface{}, result []interface{}) error {
+	q := Session.Query(table.Select()).BindStruct(query)
+	if err := q.SelectRelease(&result); err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateRow(table table.Table, query map[string]interface{}, record interface{}) error {
+	q := Session.Query(table.Update()).BindMap(query)
+	if err := q.ExecRelease(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteRow(table table.Table, query map[string]interface{}, record interface{}) error {
+	q := Session.Query(table.Delete()).BindMap(query)
+	if err := q.ExecRelease(); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ go/go.mod
@@ -0,0 +1,17 @@
+module github.com/krishak-fiem/db/go
+
+go 1.18
+
+require (
+	github.com/gocql/gocql v0.0.0-20200131111108-92af2e088537
+	github.com/krishak-fiem/utils/go v0.0.0-20220507105108-4de8a447d252
+	github.com/scylladb/gocqlx/v2 v2.7.0
+)
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/scylladb/go-reflectx v1.0.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+)
